4b9031afc6c84e9d8a8723330955f4ef3cab5103
marcoebeling2 <mebeling@usc.edu>
2025-03-04 17:06:26 -0800

    gy

--- Lab06/Lab06_ GrovePi Sensors/grovepi_sensors.py
+++ Lab06/Lab06_ GrovePi Sensors/grovepi_sensors.py
@@ -16,12 +16,31 @@ setText("")
 while True:
   try:
     # TODO:read distance value from Ultrasonic Ranger and print distance on LCD
-  
+    # read the distance from the ultrasonic senser
+    distance = grovepi.ultrasonicRead(ultrasonic_ranger)
 
     # TODO: read threshold from potentiometer
+    # read value from potentiometer
+    threshold_analog = grovepi.analogRead(potentiometer)
+    # convert the analog value to threshold value
+    max_dist = 400 # 400 cm max
+    threshold = int(threshold_analog/1023 * max_dist)
 
     
     # TODO: format LCD text according to threshhold
+    # fromat threshold appropriately
+    lcd_string = f"{threshold:4}cm"
+    # check if the distance is less than threshold
+    if distance < threshold:
+      lcd_text += "OBJ PRES" # change string if color is present
+      # change color to red
+      setRGB(255,0,0)
+    else: # if the object is not in range...
+        # keep color green
+        setRGB(0,255,0)
+
+    # Display on LCD
+    setText_norefresh(f"{lcd_text}\n{distance:4}cm")
   
     
   except IOError:
