a00aa4fe3a9abef310be675dad7aad623980f968
marcoebeling2 <mebeling@usc.edu>
2025-03-11 17:02:05 -0700

    b

--- Lab07 - PCB Design/test_suite.py
+++ Lab07 - PCB Design/test_suite.py
@@ -20,6 +20,11 @@ mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
 lux_treshold=0  # change this value
 sound_treshold=0 # change this value
 
+# light sensor channel
+light_adc = 0
+# sound sensor channel
+sound_adc = 1
+
 
 while True: 
   time.sleep(0.5) 
@@ -30,3 +35,47 @@ while True:
 
   # get reading from adc 
   # mcp.read_adc(adc_channel)
+  
+
+  # Blink led in 5 times with 500ms on/off intervals
+  for _ in range(5):
+      GPIO.output(11, GPIO.HIGH)
+      time.sleep(0.5)
+      GPIO.output(11, GPIO.LOW)
+      time.sleep(0.5)
+  
+  # read lead sensor and print out hte value
+  start_time = time.time() # get start time to measure 5 sec span
+  while time.time() - start_time < 5:
+      light_val = mcp.read_adc(light_adc)  
+      if light_val > lux_treshold: # check if brightness is bigger than the threshold
+          status = "bright"
+      else:
+          status = "dark"
+      print("Light Sensor: {} - {}".format(light_val, status))
+      time.sleep(0.1)
+  
+   # Blink led in 4 times with 200ms on/off intervals
+  for _ in range(4):
+      GPIO.output(11, GPIO.HIGH)
+      time.sleep(0.2)
+      GPIO.output(11, GPIO.LOW)
+      time.sleep(0.2)
+  
+  # read sound sensor for 5 seconds and print out the value
+  start_time = time.time() # start time to calculate when 5 seconds is up
+  led_on_until = 0  # Timestamp until which the LED should remain on
+  while time.time() - start_time < 5:
+      sound_val = mcp.read_adc(sound_adc)  # read sound adc val
+      print("Sound Sensor: {}".format(sound_val)) # print the value
+      if sound_val > sound_treshold: # if the threshold is exceeded
+          # get the start time of the led on period
+          start_time = time.time()
+          # calculate the end time
+          end_time = start_time + 0.1
+      # keep the LED on if not end time yet
+      if time.time() < end_time:
+          GPIO.output(11, GPIO.HIGH)
+      else:
+          GPIO.output(11, GPIO.LOW)
+      time.sleep(0.1)
