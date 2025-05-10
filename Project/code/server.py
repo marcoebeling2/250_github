339663bcbede6affabbe4a4bad1163134792cadf
marcoebeling2 <mebeling@usc.edu>
2025-05-09 17:40:47 -0700

    a

--- Project/code/server.py
+++ Project/code/server.py
@@ -1,12 +1,12 @@
 import os
 import pandas as pd
-import matplotlib.pyplot
+import matplotlib.pyplot as plt
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
 from typing import Any, List
-from pyngrok
+from pyngrok import ngrok, conf
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import RedirectResponse
 
